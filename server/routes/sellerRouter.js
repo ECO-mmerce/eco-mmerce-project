5db18733ab53d0aaed81441881f9e8fe7938f1f3
baskoropandu <baskoro.p.g@gmail.com>
2021-09-18 03:13:47 +0700

    done

--- server/routes/sellerRouter.js
+++ server/routes/sellerRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const upload = require('../middlewares/multer');
 const uploadImage = require('../middlewares/imagekit');
+const detectIngredients = require('../middlewares/cloudVision')
 
 const SellerController = require('../controllers/sellerController');
 const authentication = require('../middlewares/authentication');
@@ -17,7 +18,7 @@ router.post(
 router.use(authentication);
 router.use(authorization);
 router.get('/products', SellerController.getAllProducts);
-router.post('/products', SellerController.createProduct);
+router.post('/products',upload.fields([{name: 'ingredients', maxCount: 1}, {name: 'images', maxCount: 3}]), detectIngredients, SellerController.createProduct);
 router.get('/products/:id', SellerController.getProduct);
 router.put('/products/:id', SellerController.updateProduct);
 router.delete('/products/:id', SellerController.deleteProduct);
