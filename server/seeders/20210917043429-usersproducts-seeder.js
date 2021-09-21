1996997209b8fc463af7076c5f6eb8dfb51d1e25
ditaisyiyah <ditalara19@gmail.com>
2021-09-21 23:04:26 +0700

    update seeder

--- server/seeders/20210917043429-usersproducts-seeder.js
+++ server/seeders/20210917043429-usersproducts-seeder.js
@@ -17,6 +17,42 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          ProductId: 3,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          ProductId: 4,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          ProductId: 5,
+          UserId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          ProductId: 6,
+          UserId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          ProductId: 7,
+          UserId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          ProductId: 8,
+          UserId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ],
       {}
     );
