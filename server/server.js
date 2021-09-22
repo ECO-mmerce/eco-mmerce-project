9a993b6ed2b28f62a3aa732655cb712ffcfa369f
MarkTiger <marco.tiger.98@gmail.com>
2021-09-22 23:35:37 +0700

    SSL settings

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const server = require('./socketConfig');
-const http = require('./bin/http');
+const { server: http, httpsServer } = require('./bin/http');
 server.attach(http);
+server.attach(httpsServer);
 
 // nodemon server.js
